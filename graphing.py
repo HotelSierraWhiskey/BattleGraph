d36269ce993d71c28cc74beb531998106dd5dded
henry <henrywynberg@gmail.com>
2022-06-24 01:53:02 -0400

    began graphing

--- graphing.py
+++ graphing.py
@@ -1,8 +1,11 @@
 from qtstrap import *
 from parser import Parser
 from PySide2.QtWebEngineWidgets import QWebEngineView
-from plotly.graph_objects import Figure, Scatter
+from plotly.graph_objects import Figure, Pie
 import plotly
+import plotly.express as px
+
+import pandas as pd
 
 import numpy as np
 
@@ -13,18 +16,31 @@ class Graph(QWebEngineView):
 
         self.parser = Parser()
 
-        x = np.arange(1000)
-        y = x**2
+        self.fig = None
 
-        fig = Figure(Scatter(x=x, y=y))
+        self.html = ''
 
-        html = '<html><body>'
-        html += plotly.offline.plot(fig, output_type='div', include_plotlyjs='cdn')
-        html += '</body></html>'
+        self.update_graph()
 
-        self.setHtml(html)
+        self.setHtml(self.html)
 
-    def update(self, infile):
+    def update_data(self, infile):
         self.parser.infile = infile
         self.parser.load_html()
-        print(f"updating parser: {self.parser.infile}")
+        self.parser.parse_data()
+
+    def update_graph(self):
+        self.html = '<html><body>'
+
+        if self.parser.infile:
+            graph_data = self.parser.data['categories']
+            labels = list(graph_data.keys())
+            values = list(graph_data.values())
+
+            fig = Figure(data=[Pie(labels=labels, values=values)])
+
+            self.html += plotly.offline.plot(fig, output_type='div', include_plotlyjs='cdn')
+
+        self.html += '</body></html>'
+
+        self.setHtml(self.html)
