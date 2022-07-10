678c6c4c84757b8555015750348bff788c0287a3
henry <henrywynberg@gmail.com>
2022-07-10 12:15:42 -0400

    moved drag and drop behaviour to Graph

--- graphing.py
+++ graphing.py
@@ -1,36 +1,30 @@
+from operator import truediv
 from qtstrap import *
 from parser import Parser
 from PySide2.QtWebEngineWidgets import QWebEngineView
 from plotly.graph_objects import Figure, Pie
 import plotly
-import plotly.express as px
-
-import pandas as pd
-
-import numpy as np
 
 
 class Graph(QWebEngineView):
+    
+    infile_changed = Signal(str)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.setAcceptDrops(True)
         self.parser = Parser()
-
-        self.fig = None
-
         self.html = ''
+        self._update_graph()
 
-        self.update_graph()
-
-        self.setHtml(self.html)
-
-    def update_data(self, infile):
+    def _update_data(self, infile):
         self.parser.infile = infile
         self.parser.load_html()
         self.parser.parse_data()
 
-    def update_graph(self):
-        self.html = '<html><body>'
+    def _update_graph(self):
+        self.html = "<html><body>" 
 
         if self.parser.infile:
             graph_data = self.parser.data['categories']
@@ -39,8 +33,30 @@ class Graph(QWebEngineView):
 
             fig = Figure(data=[Pie(labels=labels, values=values)])
 
-            self.html += plotly.offline.plot(fig, output_type='div', include_plotlyjs='cdn')
+            self.html += plotly.offline.plot(fig, output_type='div', include_plotlyjs='cdn') 
+            # TODO: offline include_plotlyjs values don't seem to work. 'cdn' requires an internet connection. 
 
         self.html += '</body></html>'
-
         self.setHtml(self.html)
+
+    def update(self, infile):
+        self._update_data(infile)
+        self._update_graph()
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore() 
+
+    def dragMoveEvent(self, event):
+        event.acceptProposedAction()
+    
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.setDropAction(Qt.CopyAction)
+            event.accept()
+            fname = event.mimeData().urls()[0].url()
+            infile = fname.replace('file://', '')
+            self.infile_changed.emit(infile)
+            self.update(infile)
--- main_window.py
+++ main_window.py
@@ -1,4 +1,3 @@
-from bs4 import BeautifulSoup
 from qtstrap import *
 from menu import MainMenuBar
 from graphing import Graph
@@ -10,7 +9,6 @@ class MainWindow(BaseMainWindow):
 
         self.infile = None
 
-        self.setAcceptDrops(True)
         widget = QWidget(self)
         self.setCentralWidget(widget)
 
@@ -19,11 +17,9 @@ class MainWindow(BaseMainWindow):
         self.main_menu = MainMenuBar()
         self.graph = Graph()
 
-
         self.main_menu.file_submenu.infile_changed.connect(self.update_infile)
-        self.main_menu.file_submenu.infile_changed.connect(self.graph.update_data)
-        self.main_menu.file_submenu.infile_changed.connect(self.graph.update_graph)
-
+        self.main_menu.file_submenu.infile_changed.connect(self.graph.update)
+        self.graph.infile_changed.connect(self.update_infile)
 
         with CVBoxLayout(widget) as layout:
             with layout.hbox() as layout:
@@ -31,24 +27,6 @@ class MainWindow(BaseMainWindow):
                     layout.add(self.main_menu)
                     layout.add(self.current_file_label)
                     layout.add(self.graph)
-
-
-    def dragEnterEvent(self, event):
-        if event.mimeData().hasUrls():
-            event.accept()
-        else:
-            event.ignore() 
-
-    def dragMoveEvent(self, event):
-        event.acceptProposedAction()
-    
-    def dropEvent(self, event):
-        if event.mimeData().hasUrls():
-            event.setDropAction(Qt.CopyAction)
-            event.accept()
-            fname = event.mimeData().urls()[0].url()
-            self.infile = fname
-            self.current_file_label.setText(fname)
 
     def update_infile(self, fname):
         self.infile = fname
