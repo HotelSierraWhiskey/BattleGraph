3fd31818727d5a01540a4eb0f4e212523945a6fc
HotelSierraWhiskey <winstontothemoon@gmail.com>
2022-07-14 01:29:27 -0400

    added fileviewer

--- graphing.py
+++ graphing.py
@@ -1,19 +1,19 @@
-from operator import truediv
 from qtstrap import *
+from bs4 import BeautifulSoup
 from parser import Parser
 from PySide2.QtWebEngineWidgets import QWebEngineView
 from plotly.graph_objects import Figure, Pie
 import plotly
 
 
-
-class BaseGraph(QWebEngineView):
+class BaseEngineView(QWebEngineView):
 
     infile_changed = Signal(str)
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setAcceptDrops(True)
+        self.setMinimumHeight(675)
     
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
@@ -34,7 +34,19 @@ class BaseGraph(QWebEngineView):
             self.update(infile)
 
 
-class PieChart(BaseGraph):
+class FileViewer(BaseEngineView):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.html = ''
+
+    def update(self, infile):
+        with open(infile) as file:
+            soup = BeautifulSoup(file, 'html.parser')
+            self.html = str(soup)
+            self.setHtml(self.html)
+        
+
+class PieChart(BaseEngineView):
     
     from piechart import parse
     parser = Parser(parse)
@@ -42,8 +54,6 @@ class PieChart(BaseGraph):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        self.setMinimumHeight(675)
-        self.setAcceptDrops(True)
         self._update_graph()
 
     def _update_data(self, infile):
@@ -54,25 +64,16 @@ class PieChart(BaseGraph):
         self.html = '<html><body>' 
 
         if self.parser.infile:
-            labels = list(self.parser.data['categories'].keys())
-            values = []
-            text = []
-            for i in labels:
-                values.append(self.parser.data['categories'][i]['pts'])
-                text.append(self.parser.data['categories'][i]['units'])
 
+            labels = self.parser.data['labels']
+            values = self.parser.data['values']
+            text = self.parser.data['text']
             title = self.parser.data['title']
 
-            pie = Pie(labels=labels, values=values, text=text, textfont_size=12, textposition='outside')
-
-            self.fig = Figure([pie])
+            self.fig = Figure([Pie(labels=labels, values=values, text=text, textfont_size=12,textposition='outside')])
             self.fig.update_layout(title=title, font=dict(size=10), legend=dict(orientation='h', yanchor="top", xanchor="right"))
 
             self.html += plotly.offline.plot(self.fig, output_type='div', include_plotlyjs='cdn') 
-
-            # TODO: other include_plotlyjs values don't seem to work. 'cdn' works but requires an internet connection.
-            # the html produced by offline options (when output_type='file') will render plotly graphs 
-            # when loaded by a browser, but will fail to load in a QWebEngine. Or something. 
 
         self.html += '</body></html>'
         self.setHtml(self.html)
@@ -84,5 +85,4 @@ class PieChart(BaseGraph):
     def update(self, infile):
         self._update_data(infile)
         self._update_graph()
-
 
