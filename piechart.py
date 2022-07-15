44e48ff5fc7587249e6e4fa719803c300b290a63
HotelSierraWhiskey <winstontothemoon@gmail.com>
2022-07-14 22:37:27 -0400

    need more cleanup

--- piechart.py
+++ piechart.py
@@ -1,9 +1,10 @@
+import re
 from collections import Counter
 
 
 def parse(soup):
     data = {}
-    data['title'] = str(soup.find('h1').contents[0])
+    data['title'] = str(soup.find('h1').text).replace('\n', '')
     list_tags = [i for i in soup.find_all('li', class_='category')]
     data['categories'] = {}
 
@@ -12,11 +13,10 @@ def parse(soup):
         units = [t.h4.contents[0] for t in tag.find_all('li', {'class': 'rootselection'})]
 
         if 'pts' in entry:
-            entry = entry.replace('[', '').replace(']', '')
+            entry = re.sub('\[|\]', '', entry)
             entry = entry.split(' ')
 
             pts = int(entry[-1].replace('pts', ''))
-            # PL = int(entry[-3])
             slot = ''.join([i + ' ' for i in entry[:-3]]) if len(entry[:-3]) >= 2 else ''.join(entry[:-3])
 
             if not slot in data['categories']:
@@ -29,6 +29,8 @@ def parse(soup):
                 for unit in units:
                     data['categories'][slot]['units'].append(unit)
     
+    unit_names = set()
+
     for slot in data['categories'].keys():
         counter = Counter(data['categories'][slot]['units'])
 
@@ -36,10 +38,26 @@ def parse(soup):
         for k, v in counter.items():
             annotation += k + ' '
             annotation += 'x' + str(v) + '<br>'
+            # unit_names.add(k.split(' [')[0])
 
-        data['categories'][slot]['units'] = annotation[:-4]    # remove trailing linebreak
+        data['categories'][slot]['units'] = annotation[:-4] + '<br><b>total: ' + str(data['categories'][slot]['pts'])   # remove trailing linebreak
 
-    return data
+    profiles = soup.find_all('td', {'class': 'profile-name'})
+
+    labels = list(data['categories'].keys())
+    values = []
+    text = []
+    for label in labels:
+
+        values.append(data['categories'][label]['pts'])
+        text.append(data['categories'][label]['units'])
+
+    title = data['title']
+
+
+    # print(profiles)
+
+    return {'labels': labels, 'values': values, 'text': text, 'title': title}
             
     
     
